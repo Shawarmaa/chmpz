f9bc19596973bb39d88f7faa95c305ea08d5a488
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-02-19 23:03:47 +0000

    adding thinking monkey

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react";
 import { HyperText } from "@/components/magicui/hyper-text";
+import Image from "next/image";
+import { Tilt } from "@/components/ui/tilt";
 
 export default function Home() {
   const [key, setKey] = useState(0);
@@ -16,6 +18,9 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
+      <Tilt  isRevese>
+        <Image width={200} height={200} alt="Thinking monkey pic" src={"/image.png"} className="" />
+      </Tilt>
       <HyperText key={key}>Coming Soon</HyperText>
     </div>
   );
