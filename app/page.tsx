5de57bf27790f86500ac0b8924ddb213d3b3acbb
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-02-19 13:47:13 +0000

    coming soon sign

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { HyperText } from "@/components/magicui/hyper-text";
+
 
 
 export default function Home() {
   return (
-    <div>
-      <h3>coming soon</h3>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <HyperText>Comming Soon</HyperText>
     </div>
   );
 }
