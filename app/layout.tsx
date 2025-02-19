a7f8dcbf36d0d0671a177a25b4d721eef5f34428
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-02-19 14:13:36 +0000

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function RootLayout({
@@ -10,6 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
